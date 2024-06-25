ff6406d8810f595e6d9525c2a80ad298f34c3f78
king-martinien <martinienfokoue@yahoo.com>
2024-06-25 17:16:57 +0100

    Iut notify api initialization

---
+++ iut-notify-api/src/main/java/com/kingmartinien/iutnotifyapi/IutNotifyApiApplication.java
@@ -0,0 +1,13 @@
+package com.kingmartinien.iutnotifyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IutNotifyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IutNotifyApiApplication.class, args);
+    }
+
+}
---
+++ iut-notify-api/src/test/java/com/kingmartinien/iutnotifyapi/IutNotifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingmartinien.iutnotifyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IutNotifyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
